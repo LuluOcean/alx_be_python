4ddb20e8e5408f3600093413d0274c2d8ad2f3b2
BlackNate <mnyaluza.nm@gmail.com>
2025-06-06 08:42:36 +0200

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,9 @@
 def perform_operation(num1, num2, operation): 
     if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
         return f"Cannot {operation} {num1} with {num2}"
-    elif num2 == 0 and operation == "divide":
-        return f"Cannot {operation} {num1} with {num2}"
+    elif num2 == 0:
+        if operation == "divide":
+            return f"Cannot {operation} {num1} with {num2}"
     else: 
         match operation:
             case "add":
